0f7b2da210e0f1eb68999892770a0c8f1fb15eb0
Luis E. Rojas Cabrera <lrojas94@gmail.com>
2015-06-28 23:57:17 -0400

    Fixed bugs on problem 3.2

--- Problema 3.2/Problema 3.2/Program.cs
+++ Problema 3.2/Problema 3.2/Program.cs
@@ -8,18 +8,23 @@ namespace Problema_3._2
 {
     class Program
     {
-        public static bool colorBFS(List<Node> tree) {
+        static int count = 0;
+        public static bool colorBFS(Node n) {
             Color colorToPaint; //Actual color to paint.
             Stack<Node> s = new Stack<Node>();
-            tree[0].color = Color.Blue;
-            s.Push(tree[0]);
+            if(n.color == Color.None)
+                n.color = Color.Blue;
+            s.Push(n);
             while (s.Count != 0) {
                 Node parent = s.Pop();
+                if (parent.check)
+                    continue;
+                count++;//keep track of the number of operations done (Testing purposes).
                 parent.check = true;
                 if (parent.color == Color.Red)
                     colorToPaint = Color.Blue;
                 else
-                    colorToPaint = Color.Red;
+                    colorToPaint = Color.Red; 
                 for (int i = 0; i < parent.children.Count; i++) {
                     Node child = parent.children[i];
                     if (child.color == parent.color)
@@ -53,39 +58,42 @@ namespace Problema_3._2
                 int a = Convert.ToInt32(r[0]), b = Convert.ToInt32(r[1]);
                 tree[a - 1].children.Add(tree[b - 1]); //Add to children. Make a directed graph.
             }
-
-            if (colorBFS(tree)) {
-                //Print blue ones:
-                List<Node> Blues = new List<Node>(), Reds = new List<Node>();
-                for (int i = 0; i < tree.Count; i++)
-                {
-                    Node n = tree[i];
-                    if (n.color == Color.Blue)
-                        Blues.Add(n);
-                    else
-                        Reds.Add(n);
+            //Since the graph is not fully connected, better make sure to paint all nodes.
+            for (int i = 0; i < tree.Count; i++) {
+                if (!colorBFS(tree[i])) {
+                    Console.WriteLine("No solution found...");
+                    return;
                 }
-
-                Console.WriteLine(Blues.Count);
-                for (int i = 0; i < Blues.Count; i++) {
-                    Node n = Blues[i];
-                    Console.Write(n.value + " ");
-                }
-                Console.WriteLine();
-                Console.WriteLine(Reds.Count);
-                for (int i = 0; i < Reds.Count; i++)
-                {
-                    Node n = Reds[i];
-                    Console.Write(n.value + " ");
-                }
-                Console.WriteLine();
             }
-            else
+
+            //Print blue ones:
+            List<Node> Blues = new List<Node>(), Reds = new List<Node>();
+            for (int i = 0; i < tree.Count; i++)
             {
-                Console.WriteLine("No solution found.");
+                Node n = tree[i];
+                if (n.color == Color.Blue)
+                    Blues.Add(n);
+                else
+                    Reds.Add(n);
             }
 
+            Console.WriteLine(Blues.Count);
+            for (int i = 0; i < Blues.Count; i++)
+            {
+                Node n = Blues[i];
+                Console.Write(n.value + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine(Reds.Count);
+            for (int i = 0; i < Reds.Count; i++)
+            {
+                Node n = Reds[i];
+                Console.Write(n.value + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Number of ops: " + count);
             Console.Read();
+
             
         }
     }
---
+++ Problema 3.3/Problema 3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Read both element's lists.
+            int aSize = Convert.ToInt32(Console.ReadLine());
+            String[] aList = Console.ReadLine().Split(' ');
+            int bSize = Convert.ToInt32(Console.ReadLine());
+            String[] bList = Console.ReadLine().Split(' ');
+
+        }
+    }
+}

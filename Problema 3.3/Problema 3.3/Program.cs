8f89ad290619b4db995d9857dac9e7aec8b9437d
Luis E. Rojas Cabrera <lrojas94@gmail.com>
2015-06-29 00:19:37 -0400

    Done with P. 3.3

--- Problema 3.3/Problema 3.3/Program.cs
+++ Problema 3.3/Problema 3.3/Program.cs
@@ -12,9 +12,37 @@ namespace Problema_3._3
         {
             //Read both element's lists.
             int aSize = Convert.ToInt32(Console.ReadLine());
-            String[] aList = Console.ReadLine().Split(' ');
+            String[] elementsA = Console.ReadLine().Split(' ');
             int bSize = Convert.ToInt32(Console.ReadLine());
-            String[] bList = Console.ReadLine().Split(' ');
+            String[] elementsB = Console.ReadLine().Split(' ');
+
+            //Arrays to check in O(1);
+            bool[] listA = new bool[bSize + aSize], listB = new bool[bSize + aSize];
+            for (int i = 0; i < elementsA.Length; i++) {
+                int n = Convert.ToInt32(elementsA[i]) - 1;
+                listA[n] = true;
+            }
+            for (int i = 0; i < elementsB.Length; i++)
+            {
+                int n = Convert.ToInt32(elementsB[i]) - 1;
+                listB[n] = true;
+            }
+
+            int restrictions = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < restrictions; i++) {
+                String[] restriction = Console.ReadLine().Split(' ');
+                int ra = Convert.ToInt32(restriction[0]) - 1, rb = Convert.ToInt32(restriction[1]) - 1;
+                //Check if they are in list A.
+                if ((listA[ra] && listA[rb]) || (listB[ra] && listB[rb]))
+                {
+                    Console.WriteLine("Wrong Answer");
+                    return;
+                }
+
+            }
+
+            Console.WriteLine("Accepted!");
+            Console.Read();
 
         }
     }
